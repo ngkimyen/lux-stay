ea2aeb89d87a3139c88dcbaf89a183345628762b
Jasmine Nguyen <ngkimyen7901@gmail.com>
2021-10-10 14:17:36 +0700

    first commit

---
+++ assets/JS/base.js
@@ -0,0 +1,269 @@
+const render = {
+    outstanding : [
+        {
+            id: 1,
+            img: './assets/img/outstanding-1.png',
+            name: 'TP. Hồ Chí Minh',
+            quantity : 1878 ,
+        }, 
+        {
+            id: 2,
+            img: './assets/img/outstanding-8.png',
+            name: 'Hà Nội',
+            quantity : 2543 ,
+        }, 
+        {
+            id: 3,
+            img: '../assets/img/outstanding-3.png',
+            name: 'Đà Lạt',
+            quantity : 1478 ,
+        }, 
+        {
+            id: 4,
+            img: '../assets/img/outstanding-4.png',
+            name: 'Đà Nẵng',
+            quantity : 478 ,
+        }, 
+        {
+            id: 5,
+            img: '../assets/img/outstanding-5.png',
+            name: 'Nha Trang',
+            quantity : 629 ,
+        }, 
+        {
+            id: 6,
+            img: '../assets/img/outstanding-6.png',
+            name: 'Quảng Ninh',
+            quantity : 478 ,
+        }, 
+        {
+            id: 7,
+            img: '../assets/img/outstanding-7.png',
+            name: 'Hội An',
+            quantity : 178 ,
+        }, 
+        {
+            id: 8,
+            img: '../assets/img/outstanding-2.png',
+            name: 'Vũng Tàu',
+            quantity : 778 ,
+        }, 
+    
+    ],
+    suggestions : [
+        {
+            id: 1,
+            img : './assets/img/suggest-1.jpeg',
+            heading: ' VI VU NGOẠI THÀNH HÀ NỘI',
+            description: 'Trải nghiệm không gian thoáng đãng cho chuyến đi ngay gần Hà Nội'
+        },
+        {
+            id: 2,
+            img : './assets/img/suggest-2.jpeg',
+            heading: ' VŨNG TÀU BIỆT THỰ HỒ BƠI',
+            description: 'Những căn biệt thự có hồ bơi dành cho kỳ nghỉ của bạn tại Vũng Tàu'
+        },
+        {
+            id: 3,
+            img : './assets/img/suggest-3.jpeg',
+            heading: ' HÀ NỘI NỘI THÀNH LÃNG MẠNG',
+            description: 'Không gian lãng mạn dành cho cặp đôi tại trung tâm Hà Nội'
+        },
+        {
+            id: 4,
+            img : './assets/img/suggest-4.jpeg',
+            heading: ' SÀI GÒN CẦN LÀ CÓ NGAY',
+            description: 'Những căn homestay có 01 phòng ngủ tại Sài Gòn có thể đặt nhanh chóng'
+        },
+        {
+            id: 5,
+            img : './assets/img/suggest-5.jpeg',
+            heading: ' BỂ BƠI & BBQ',
+            description: 'Trải nghiệm đẳng cấp tại những căn homestay có bể bơi đẹp và khu vực BBQ ấm cúng.'
+        },
+        {
+            id: 6,
+            img : './assets/img/suggest-6.jpeg',
+            heading: ' SIÊU GIẢM GIÁ ',
+            description: 'Top chỗ ở giảm giá đến 50% từ các chủ nhà thân thiện trên Luxstay.'
+        },
+        {
+            id: 7,
+            img : './assets/img/suggest-7.jpeg',
+            heading: 'GẦN TRUNG TÂM ',
+            description: 'Dễ dàng di chuyển khắp nơi với top chỗ ở khu vực trung tâm thành phố Hồ Chí Minh'
+        }
+    ],
+    explores: [
+        {
+            id: 1,
+            img: './assets/img/explore-1.jpeg',
+            heading: '5 resort hạng sang ở Việt Nam xuất hiện trên tạp chí du lịch Anh',
+            topic: 'Thông tin homestay'
+        },
+        {
+            id: 2,
+            img: './assets/img/explore-2.jpeg',
+            heading: 'Du lịch Cần Thơ nhất định phải ghé thăm những địa điểm này',
+            topic: 'Thông tin homestay'
+        },
+        {
+            id: 3,
+            img: './assets/img/explore-3.jpeg',
+            heading: 'Khám phá 5 resort sang chảnh tại khu du lịch Hồ Tràm',
+            topic: 'Thông tin khách sạn'
+        },
+        {
+            id: 4,
+            img: './assets/img/explore-4.jpeg',
+            heading: '3 resort " thiên đường " tại Côn Đảo cho 1 kì nghỉ tuyệt vời',
+            topic: 'Thông tin homestay'
+        },
+        {
+            id: 5,
+            img: './assets/img/explore-5.jpeg',
+            heading: 'Dịch vụ khách sạn kì lạ nhất trên thế giới ',
+            topic: 'Thông tin khách sạn'
+        },
+        {
+            id: 6,
+            img: './assets/img/explore-6.jpeg',
+            heading: 'Khách sạn tại Thổ Nhĩ Kì xứng đáng với danh hiệu khách sạn sang chảnh nhất thế giới',
+            topic: ''
+        },
+    ],
+    usage : [
+        {
+            id: 1,
+            img : './assets/img/usage-1.jpeg'
+        },
+        {
+            id: 2,
+            img : './assets/img/usage-2.jpeg'
+        },
+        {
+            id: 3,
+            img : './assets/img/usage-3.jpeg'
+        },
+        {
+            id: 4,
+            img : './assets/img/usage-4.jpeg'
+        },
+        {
+            id: 5,
+            img : './assets/img/usage-5.jpeg'
+        },
+        
+    ]
+}
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const body = $('body');
+
+// ---------------RENDER 
+const outstandingPlaces = $('.outstanding__place');
+const eventsWrap = $('.events-wrap');
+const suggestPlaces = $('.suggestion__place');
+const explorePlace = $('.explore__slider');
+const usageSection = $('.usage__slider');
+render.outstanding.forEach( (place) => {
+    outstandingPlaces.innerHTML += `
+    <div class="col l-2-4 m-4 c-5 outstanding__place-item slide">
+        <div class="outstanding__img-wrap">
+            <img src="${place.img}" alt="" class="outstanding_img ">
+        </div>
+        <div class="outstanding__info">
+            <h4 class="outstanding__name">${place.name}</h4>
+            <p class="outstanding__quantity">
+                <b>${place.quantity}</b>
+                Chỗ ở
+            </p>
+        </div>
+    </div>
+    `
+})
+
+render.suggestions.forEach( (suggestion) => {
+    suggestPlaces.innerHTML += `
+    <a href="" class="col l-3 m-6 c-11 suggestion__item slide">
+        <div class="suggestion__img">
+            <img src="${suggestion.img}" alt="">
+        </div>
+        <div class="suggestion__info">
+            <h4 class="suggestion__heading">${suggestion.heading}</h4>
+            <p class="suggestion__description">
+                ${suggestion.description}
+            </p>
+        </div>
+    </a>
+    
+    `
+})
+
+render.explores.forEach( (explore) => {
+    explorePlace.innerHTML += `
+    <a href="" class="col l-4 m-6 c-11 explore__item slide">
+        <div class="explore__img">
+            <img src="${explore.img}" alt="">
+        </div>
+        <div class="explore__topic">${explore.topic.toUpperCase()} </div>
+        <div class="explore__heading">${explore.heading}</div>
+    </a> 
+    `
+})
+
+render.usage.forEach( (usage) => {
+    usageSection.innerHTML += `
+    <a href="" class="col l-2-4 m-4 c-11 usage__item slide">
+        <div class="usage__img">
+            <img src="${usage.img}" alt="">
+        </div>
+    </a> 
+    `
+})
+
+
+
+
+
+// ---------------- BASE FUNCTION
+function display(element,displayName) {
+    if (!displayName) {return element.classList.add('active')}
+    else return element.classList.add(`active-${displayName}`)
+}
+
+function hide(element,displayName) {
+    if (!displayName) {return element.classList.remove('active')}
+        else return element.classList.remove(`active-${displayName}`)
+}
+
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+function handleDisplay(element, animationName, displayName, headerBtn) {
+    element.style.animation= `${animationName} 0.2s linear`;
+    if (headerBtn) headerBtn.classList.add('isClicked');
+    display(element,displayName);
+}
+
+function handleHide (target, element, animationName, displayName, headerBtn) {
+    if (!target.closest(`${element.className}`)) {
+        element.style.animation= `${animationName} 0.3s linear`;
+        setTimeout(() => {
+            hide(element,displayName)
+        }, 280);
+    }
+    if (headerBtn) headerBtn.classList.remove('isClicked')
+}
+
+function removeClassList (elements, start, className) {
+    for ( let i = start; i < elements.length; i++) 
+        elements[i].classList.remove(`${className}`)
+}
+
+function addClassList (elements, start, end , className) {
+    for ( let i = start; i <= end; i++) 
+        elements[i].classList.add(`${className}`)
+}   
---
+++ assets/JS/main.js
@@ -0,0 +1,401 @@
+
+
+(function handleHeader () {
+    const quantity=$('.quantity');
+    const searchQuantity = $('.search__quantity');
+    const increments = $$('.quantity__incr');
+    const decrements = $$('.quantity__decr');
+    const quantityInput = $$('.quantity__input');
+    const dateControl = $('.search__date');
+    const calendar = $('.calendar');
+
+    
+    const presentItems = $$('#month__present .day__item')
+    const nextItems = $$('#month__next .day__item')
+
+    // display date control
+    dateControl.onclick = function(e) {
+        e.stopPropagation();
+        if (e.target === this) {
+            handleDisplay(calendar,'fadeIn','block',dateControl)
+            // in case quantity is opened, hide it
+            handleHide(e.target, quantity, "fadeOut", "block",searchQuantity);
+            let date = new Date();
+            handleCalendarChange(date);
+            handleNextPrevBtn(date);
+            handlePicker();
+        }   
+    }
+    // display quantity control
+    searchQuantity.onclick = (e) => {
+        e.stopPropagation();
+        handleDisplay(quantity,'fadeIn','block',searchQuantity)
+        handleQuantityChange(increments,decrements,quantityInput)
+        // in case calender is opened, hide it
+        handleHide(e.target, calendar, "fadeOut", "block", dateControl);
+    }
+
+    function handleQuantityChange(increments,decrements,quantityInput) {
+        const deleteBtn = $('.quantity__delete');
+        let counter;
+
+        increments.forEach( (increment,index) => {
+            increment.onclick = () => {
+                if (quantityInput[index].value === '0') {
+                    counter = 1
+                } else counter = parseInt(quantityInput[index].value)+1;
+                decrements[index].classList.add('active');
+                quantityInput[index].value = counter ;
+            }
+        })
+
+        decrements.forEach ((decrement,index) => {
+            decrement.onclick = () => {
+                if (decrement.classList.contains('active')) {
+                    counter = parseInt(quantityInput[index].value)-1;
+                    if (counter === 0 ) {
+                        decrement.classList.remove('active');
+                        quantityInput[index].value = counter
+                    } else quantityInput[index].value = counter;
+                }
+            }
+        })
+
+        deleteBtn.onclick = () => {
+            quantityInput.forEach ( (input,index) => {
+                input.value = 0
+                decrements[index].classList.remove('active');
+            })
+        }
+
+
+    }
+
+    function handleCalendarChange (date) {
+        const presentMonthElement = $('#month__present');
+        const nextMonthElement = $('#month__next');
+
+        const presentMonthText = $('#month__present .head__month');
+        const presentYearText = $('#month__present .head__year');
+        const nextMonthText = $('#month__next .head__month');
+        const nextYearText = $('#month__next .head__year');
+
+        const calendarDayPresent = $$('#month__present .day__item');
+        const calendarDayNext = $$('#month__next .day__item');
+
+        // -------PRESENT MONTH
+        let presentMonth = date.toLocaleString('en-us', { month: 'long' });
+        let presentYear = date.toLocaleString('en-us', { year: 'numeric' });
+            // render heading present month
+        presentMonthText.innerHTML = presentMonth;
+        presentYearText.innerHTML = presentYear;
+            // render days in present month
+        let presentDate = new Date(`${presentYear} ${presentMonth}`);
+        let quantityDayInPresentMonth = daysInMonth(date.getMonth()+1, date.getFullYear())
+        renderDaysInMonth(calendarDayPresent, presentDate.getDay()-1, quantityDayInPresentMonth,presentDate.getDay()-1 + date.getDate());
+        
+        // ------NEXT MONTH
+        let nextYear;
+        let nextMonthNumber;
+        let nextMonthNumberFromPresent = date.getMonth()+1;
+        console.log('next month :', nextMonthNumberFromPresent)
+        if (nextMonthNumberFromPresent <= 11) {    
+            nextMonthNumber = nextMonthNumberFromPresent +1 ;
+            nextYear = presentYear;
+        } else {
+            nextMonthNumber = 1;
+            nextYear = parseInt(presentYear);
+            nextYear +=1;
+        }
+            // render heading next month
+        let nextDate = new Date (`${nextYear} ${nextMonthNumber}`);
+        let quantityDayInNextMonth = daysInMonth(nextDate.getMonth()+1, nextDate.getFullYear())
+        let nextMonth = nextDate.toLocaleString('en-us', { month: 'long' });
+
+        nextYearText.innerHTML = nextYear;
+        nextMonthText.innerHTML = nextMonth ;
+            // render days in next month
+        renderDaysInMonth(calendarDayNext,nextDate.getDay()-1,quantityDayInNextMonth,nextDate.getDay()-1 + nextDate.getDate() )
+        
+        function renderDaysInMonth (elements, startDate, quantityDayInMonth, presentDate ) {
+            let counter =0;
+            resetCalender(elements);
+            hideLastRow();
+            
+            for (i = startDate ; i < elements.length ; i++) {
+                if (counter < quantityDayInMonth)   
+                    if (elements[i].classList.contains('hide')) {
+                        displayLastRow()
+                        counter++;
+                        elements[i].innerHTML = counter;
+                    } else {
+                        counter++;
+                        elements[i].innerHTML = counter;
+                    }
+            }
+            handleDateColor(elements, startDate, presentDate);
+        }
+        
+        function handleDateColor (elements, startDate, presentDate) {
+            for (let i = startDate ; i < elements.length ; i++) 
+                if (elements[i].innerHTML) 
+                    if (i >= presentDate -1) {
+                        elements[i].classList.add('active'); 
+                    } else elements[i].classList.add('expired')
+        }
+        
+        function resetCalender (elements) {
+            for (let i = 0 ; i < elements.length ; i++) {
+                elements[i].innerHTML = ""
+                elements[i].classList.remove('active');
+                elements[i].classList.remove('expired');
+            }
+        }
+        function hideLastRow () {
+            for (let i=35; i <= 41 ; i++) {
+                presentItems[i].classList.add('hide');
+                nextItems[i].classList.add('hide');
+            }
+        }
+        function displayLastRow () {
+            for (let i=35; i <= 41 ; i++) {
+                presentItems[i].classList.remove('hide');
+                nextItems[i].classList.remove('hide');
+            }
+        }
+    }
+
+    function handleNextPrevBtn(date) {
+        const next = $('.month__next-btn ');
+        const prev = $('.month__prev-btn ');
+        let counterNext = 0;
+        let counterPrev = 0;
+        let yearCounterNext = 1;
+        let yearCounterPrev = 1;
+
+        next.onclick = (e) => {
+            e.stopPropagation();
+            let nextMonth;
+            let year;
+            let month;
+            removeClassList($$('.day__item.active'),0,'isPicked');
+
+            if (counterNext === 0) {
+                month = date.getMonth();
+            } else month = date.getMonth() + counterNext;
+
+            if (month >= 11) {
+                let divisionBy12 = yearCounterNext % 12;
+                if (divisionBy12 <= 0) {
+                    nextMonth = yearCounterNext;
+                    year = date.getFullYear() +1 ;
+                } else {
+                    year =(date.getFullYear()+1 ) + Math.floor(yearCounterNext/12) 
+                    nextMonth = divisionBy12;
+                }
+                yearCounterNext++
+            } else {
+                nextMonth = month + 2;
+                year = date.getFullYear();
+            }
+            // console.log(year)
+            // console.log(nextMonth)
+            // console.log(new Date(`${year} ${nextMonth}`));
+            handleCalendarChange(new Date(`${year} ${nextMonth}`));
+            counterPrev =0;
+            return counterNext++;
+        }
+        prev.onclick = (e) => {
+            e.stopPropagation();
+            let prevMonth;
+            let year;
+            let month;
+            removeClassList($$('.day__item.active'),0,'isPicked');
+            if (counterPrev === 0) {
+                month = date.getMonth();
+            } else month = date.getMonth() - counterPrev;
+
+            if (month <= 0) {
+                let divisionBy12 = yearCounterPrev % 12;
+                if (divisionBy12 <= 0) {
+                    prevMonth = yearCounterPrev;
+                    year = date.getFullYear() +1;
+                } else {
+                    year =(date.getFullYear() + 1) - Math.floor(yearCounterPrev/12) 
+                    prevMonth = divisionBy12;
+                }
+                yearCounterPrev++
+            } else {
+                prevMonth = month ;
+                year = date.getFullYear();
+            }
+            // console.log(year)
+            console.log(prevMonth)
+            console.log(counterPrev )
+            handleCalendarChange(new Date(`${year} ${prevMonth}`));
+            counterNext=0;
+            return counterPrev++;
+        }
+
+    }
+
+    function handlePicker() {
+        const calendarDelete = $('.calendar__delete');
+        const dayItems = $$('.day__item.active');
+
+        let counter = 0;
+        let current=0;
+        dayItems.forEach((item, index) => {
+            item.onclick = (e) => {
+                e.stopPropagation();
+                counter++;
+                if (counter > 1) 
+                   { if (current > index) {
+                        removeClassList(dayItems,0,'isPicked')
+                        item.classList.add('isPicked')
+                    } else if (current < index) {
+                    addClassList(dayItems,current,index,'isPicked')
+                    } 
+                } else if (counter === 1)  item.classList.add('isPicked')
+               return current = index;
+            }
+        })
+
+        calendarDelete.onclick = (e) => {
+            removeClassList(dayItems,0,'isPicked')
+        }
+    
+    }
+    
+    // money change section
+    const navMoney = $('.nav__money');
+    const moneyOption = $('.money__option');
+    navMoney.onclick = (e) => {
+        e.stopPropagation();
+        handleDisplay(moneyOption,'floatIn','flex')
+    }
+
+    window.onclick = (e) => {
+        handleHide(e.target, calendar, "fadeOut", "block", dateControl);
+        handleHide(e.target, quantity, "fadeOut", "block",searchQuantity );
+        handleHide(e.target, moneyOption, "floatOut", "flex");
+    }
+    
+})();
+
+(function handleShowBanner () {
+    const banners = $$('.banner');
+    const dots = $$('.banner__dot');
+
+    dots.forEach((dot,index) => {
+        dot.onclick = (e) => {
+            console.log(index)
+            for (let i = 0; i < banners.length ; i++) hide(banners[i]);
+            display(banners[index])
+            for (let i = 0; i < dots.length ; i++) hide(dots[i]);
+            display(dot)
+            return counter = index +1 ;
+        }
+    })
+
+    let counter=0;
+    setInterval( () => {
+        for (let i = 0; i < banners.length ; i++) hide(banners[i]);
+        display(banners[counter])
+        for (let i = 0; i < dots.length ; i++) hide(dots[i]);
+        display(dots[counter])
+        if (counter<1) {
+            counter++
+        } else counter=0;
+    },4000)
+
+  
+})();
+
+function handleShowSlides (slider, numberDisplay) {
+    // click next / prev
+    const next = slider.querySelector('.slides__next.active');
+    const prev = slider.querySelector('.slides__prev');
+    const slides = slider.querySelectorAll(`.slide`);
+    let counter =0;
+    let max = slides.length - numberDisplay ;
+    let permove = 100 / numberDisplay;
+
+    next.onclick = () => {
+        counter++;
+        if (counter > 0) prev.classList.add('active') 
+        if (counter === max)  next.classList.remove('active')
+        slides.forEach(( slide ) => {
+            if (counter===0) {slide.style.left='0px'}
+            if (counter<= max) {slide.style.left=`-${counter * permove}%`}
+            if (counter > max) counter = max;
+        })
+    }
+
+    prev.onclick = () => {
+        counter--;
+        if (counter < max) next.classList.add('active')
+        if (counter === 0) prev.classList.remove('active')
+        slides.forEach(( slide ) => {
+            if (counter===0) {slide.style.left='0px'}
+            if (counter<= max) {slide.style.left=`-${counter * permove}%`}
+            if (counter < 0) counter = 0;
+        })
+    }
+}
+
+(function handleModal () {
+    const modal = $('modal')
+    const bar = $('.mb-tb__nav')
+    const nav = $('.modal__nav')
+    const close = $('.nav__close')
+    const overlay = $('.overlay')
+    // DISPLAY & HIDE MODAL
+    bar.onclick = () => {
+        handleDisplay(modal,'fadeIn','block')
+    }
+    close.onclick = () => {
+        modal.style.animation= `fadeOut .3s linear`;
+        setTimeout(() => {
+            hide(modal,'block')
+        }, 280);
+    }
+    overlay.onclick = () => {
+        modal.style.animation= `fadeOut .3s linear`;
+        setTimeout(() => {
+            hide(modal,'block')
+        }, 280);
+    }
+
+    // DISPLAY & HIDE MONEY UNIT
+    const navMoney = $('modal .nav__money')
+    const moneyOption = $('modal .money__option')
+    console.log(moneyOption)
+
+    navMoney.onclick = (e) => {
+        e.stopPropagation();
+        moneyOption.classList.toggle('active-block')
+        
+    }
+
+})()
+
+
+// responsive   
+const tablet = window.matchMedia("(min-width:740px) and (max-width: 1023px)");
+const mobile = window.matchMedia("(max-width: 739px)");
+
+if (tablet.matches)   {
+    handleShowSlides(outstandingPlaces,3)
+    handleShowSlides(eventsWrap,2)
+    handleShowSlides(suggestPlaces,2)
+    handleShowSlides(explorePlace,2)
+    handleShowSlides(usageSection,3)
+    
+} else {
+    handleShowSlides(outstandingPlaces,5)
+    handleShowSlides(suggestPlaces,4)
+    handleShowSlides(explorePlace,3)
+    handleShowSlides(usageSection,5)
+}
+
